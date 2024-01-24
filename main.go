6dbd69b725d098dc0dbfa56cf53935baa192a773
Audience-Kayela <akayela22@gmail.com>
2024-01-24 07:10:17 +0200

    looping through arrays with counter and range

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	// Arrays with Zero Values
 	var notes [7]string
 	var primes [7]int
+
+	// Adding values to arrays
 	notes[0] = "do"
 	notes[1] = "re"
 	notes[2] = "mi"
@@ -19,6 +22,8 @@ func main() {
 	primes[4] = 11
 	primes[5] = 13
 	primes[6] = 17
+
+	// Array literals
 	notes2 := [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
 	primes2 := [7]int{2, 3, 5, 7, 11, 13, 17}
 	strings := [3]string{
@@ -26,11 +31,29 @@ func main() {
 		"of long strings which",
 		"would look awkward if place in one line",
 	}
+
+	// Looping through arrays
 	for i := 0; i < len(notes); i++ {
 		fmt.Printf("%d. %s %d\n", i, notes[i], primes[i])
 
 	}
+
+	// Looping through arrays with range
+
+	for i, note := range notes2 {
+		fmt.Println(i, note)
+	}
 	fmt.Println(notes2)
 	fmt.Println(primes2)
 	fmt.Println(strings)
+
+	drive := [4]string{
+		"i",
+		"love",
+		"to",
+		"code",
+	}
+	for _, word := range drive {
+		fmt.Println(word)
+	}
 }
